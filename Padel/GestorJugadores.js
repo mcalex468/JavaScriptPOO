080866d9659802bf0a0a298dbf40c44869d5b2aa
AlexMartin <alex.martin.7e7@itb.cat>
2025-04-06 17:30:10 +0200

    commit padel

--- Padel/GestorJugadores.js
+++ Padel/GestorJugadores.js
@@ -14,13 +14,14 @@ export class GestionJugadores {
 
     // Método para eliminar un jugador por nombre
     eliminarJugador(nombreJugador) {
-        if (typeof nombreJugador == String) {
-            this.jugadoresLista = this.jugadoresLista.findIndex(jugador => jugador.nombre === nombreJugador)
+        if (typeof nombreJugador === "string") {
+            let index = this.jugadoresLista.findIndex(jugador => jugador.nombre === nombreJugador);
             if (index !== -1) {
                 this.jugadoresLista.splice(index, 1);
             }
         }
     }
+
     /*
     eliminarJugador(nombreJugador) {
         if (typeof nombreJugador == String) {
@@ -31,22 +32,42 @@ export class GestionJugadores {
 
     // Método para listar jugadores ordenados alfabéticamente
     listarJugadores() {
-        // TODO: Implementar ordenación
+        return this.jugadoresLista.sort((a, b) => a.nombre.localeCompare(b.nombre));
     }
 
+
+    /* COPIA
+    listarJugadores() {
+    return [...this.jugadoresLista].sort((a, b) => a.nombre.localeCompare(b.nombre));
+    }
+    */
+
     // Método para obtener jugadores de un nivel específico
-    jugadoresPorNivel(nivel) {
-        // TODO: Implementar filtrado por nivel
+    jugadoresPorNivel() {
+        let listaJugadoresNivel = this.jugadoresLista.sort((a, b) => b.nivel - a.nivel);
+        return listaJugadoresNivel;
     }
 
     jugadoresPorValoracionMedia() {
-        // TODO: Implementar filtrado por valoración
+        let jugadoresMedia = this.jugadoresLista.sort((a, b) => {
+            let valA = a.calcularValoracionMedia()
+            let valB = b.calcularValoracionMedia()
+            return valB - valA
+        });
+        return jugadoresMedia;
     }
 
     // Método para obtener el mejor jugador según victorias
     mejorJugador() {
-        // TODO: Implementar búsqueda del mejor jugador
+        return this.jugadoresLista.reduce((mejor, jugador) =>
+            (jugador.victorias > mejor.victorias ? jugador : mejor), this.jugadoresLista[0]);
     }
+    /*
+    mejorJugador() {
+        return this.jugadoresLista.sort((a, b) => b.victorias - a.victorias);
+    }
+    */
+
 
 
 }
